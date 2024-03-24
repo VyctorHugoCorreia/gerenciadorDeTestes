af28d4621bffbbb81bba7ddf15d66c6ae8a93225
“VyctorHugoCorreia” <vyctorhugoc@gmail.com>
2024-03-24 01:23:43 -0300

    login

--- src/screens/LoginForm.tsx
+++ src/screens/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginService from '../services/LoginService'; 
 import { setAuthToken } from '../authentication/token'; 
 import { setAuthentication } from '../authentication/authentication'; 
@@ -6,28 +6,36 @@ import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import '../styles/login.css'
 import { Button, TextField } from '@mui/material';
 
-
-
-interface LoginFormProps {
-}
+interface LoginFormProps {}
 
 const LoginForm: React.FC<LoginFormProps> = () => {
   const navigate = useNavigate();
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string>('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+
+  // Verificar se ambos os campos estão preenchidos
+  useEffect(() => {
+    setIsButtonDisabled(!(username && password));
+  }, [username, password]);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setAuthentication(false);
     try {
-        const response = await LoginService.postLogin(username, password);
-        const { token } = response;
-        setAuthToken(token);
-        setAuthentication(true);
-        navigate('/');
-    } catch (err) {
-     
+      const response = await LoginService.postLogin(username, password);
+      const { token } = response;
+      setAuthToken(token);
+      setAuthentication(true);
+      navigate('/');
+    } catch (err:any) {
+      if (Array.isArray(err) && err.length > 0) {
+        setError(err[0]); 
+      } else {
+        setError('Não foi possível realizar login, tente novamente mais tarde.');
+      }
     }
   };
 
@@ -52,19 +60,19 @@ const LoginForm: React.FC<LoginFormProps> = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+        {error && <p className='error-message'>{error}</p>}
         <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Login
-          </Button>
+          className='button-login'
+          variant="contained"
+          color="primary"
+          type="submit"
+          disabled={isButtonDisabled} 
+        >
+          Login
+        </Button>
       </form>
     </div>
   );
-  
-  
-  
-  };  
+};
 
 export default LoginForm;
